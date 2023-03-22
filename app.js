21588451b5c0a60f20d45b8130c6bb9df9ffc77c
Deukhwa <deukhwa.jeong@student.sl.on.ca>
2023-03-22 16:05:59 -0400

    add env port

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const fs = require("fs");
-const path = require("path");
+const express = require('express')
+const mongoose = require('mongoose')
+const fs = require('fs')
+const path = require('path')
 
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
-const HttpError = require("./models/http-error");
-const MONGO_DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.enwwly5.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
+const placesRoutes = require('./routes/places-routes')
+const usersRoutes = require('./routes/users-routes')
+const HttpError = require('./models/http-error')
+const MONGO_DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.enwwly5.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
 
-  next();
-});
+  next()
+})
 
-app.use("/api/places", placesRoutes);
-app.use("/api/users", usersRoutes);
+app.use('/api/places', placesRoutes)
+app.use('/api/users', usersRoutes)
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
-  throw error;
-});
+  const error = new HttpError('Could not find this route.', 404)
+  throw error
+})
 
 app.use((error, req, res, next) => {
   if (req.file) {
     fs.unlink(req.file.path, (err) => {
-      console.log(err);
-    });
+      console.log(err)
+    })
   }
   if (res.headerSent) {
-    return next(error);
+    return next(error)
   }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
-});
+  res.status(error.code || 500)
+  res.json({ message: error.message || 'An unknown error occurred!' })
+})
 
 mongoose
   .connect(MONGO_DB_URI)
   .then(() => {
-    app.listen(5050);
+    app.listen(process.env.PORT || 5050)
   })
   .catch((err) => {
-    console.log(err);
-  });
+    console.log(err)
+  })
